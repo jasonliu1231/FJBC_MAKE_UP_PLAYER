8dce7f27da885100dc6a7cdd791d61312b946a9c
jasonliu1231 <liuzaiyao1231@gmail.com>
2025-02-13 09:35:38 +0800

    修改登入畫面

--- app/page.js
+++ app/page.js
@@ -46,111 +46,107 @@ export default function Example() {
   }
 
   return (
-    <>
-      <div className="container mx-auto p-12">
-        <div className="min-h-full mt-36 grid grid-cols-2">
-          <div className="col-span-1 flex justify-center py-12">
-            <div className="mt-10 w-full px-12">
-              <div>
-                <img
-                  alt="Your Company"
-                  src="/FJBC_Logo.png"
-                  className="h-10 w-auto"
-                />
-                <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">補課系統</h2>
-              </div>
-              <div>
-                <div className="space-y-4">
-                  <div>
-                    <label
-                      htmlFor="username"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      帳號
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        value={info?.username || ""}
-                        onChange={(event) => {
-                          setInfo({
-                            ...info,
-                            username: event.target.value
-                          });
-                        }}
-                        id="username"
-                        name="username"
-                        type="text"
-                        required
-                        className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
-                      />
-                    </div>
+    <div className="container mx-auto p-12">
+      <div className="min-h-full mt-36 grid grid-cols-3">
+        <div className="col-span-1 flex justify-center">
+          <div className="mt-10 w-full px-12">
+            <div className="flex items-center">
+              <img
+                alt="Your Company"
+                src="/FJBC_Logo.png"
+                className="h-10 w-auto"
+              />
+              <h2 className="text-xl font-bold text-gray-900">補課系統</h2>
+            </div>
+            <div>
+              <div className="space-y-4">
+                <div className="mt-4">
+                  <label
+                    htmlFor="username"
+                    className="block text-lg font-medium leading-6 text-gray-900"
+                  >
+                    帳號
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={info?.username || ""}
+                      onChange={(event) => {
+                        setInfo({
+                          ...info,
+                          username: event.target.value
+                        });
+                      }}
+                      id="username"
+                      name="username"
+                      type="text"
+                      required
+                      className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
+                    />
                   </div>
+                </div>
 
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      密碼
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        value={info?.password || ""}
-                        onChange={(event) => {
-                          setInfo({
-                            ...info,
-                            password: event.target.value
-                          });
-                        }}
-                        id="password"
-                        name="password"
-                        type="password"
-                        required
-                        autoComplete="current-password"
-                        className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
-                      />
-                    </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-lg font-medium leading-6 text-gray-900"
+                  >
+                    密碼
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={info?.password || ""}
+                      onChange={(event) => {
+                        setInfo({
+                          ...info,
+                          password: event.target.value
+                        });
+                      }}
+                      id="password"
+                      name="password"
+                      type="password"
+                      required
+                      autoComplete="current-password"
+                      className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
+                    />
                   </div>
+                </div>
 
-                  <div>
-                    {loginLoading ? (
-                      <div className="flex justify-center items-center">
-                        <div className="spinner"></div>
-                        <div>
-                          <div className="mx-4 text-blue-500">登入中，請稍候...</div>
-                        </div>
+                <div>
+                  {loginLoading ? (
+                    <div className="flex justify-center items-center">
+                      <div className="spinner"></div>
+                      <div>
+                        <div className="mx-4 text-blue-500">登入中，請稍候...</div>
                       </div>
-                    ) : (
-                      <button
-                        onClick={login}
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        登入
-                      </button>
-                    )}
-                  </div>
+                    </div>
+                  ) : (
+                    <button
+                      onClick={login}
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      登入
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-span-1 flex justify-center items-center">
-            <div className="w-1/2">
-              <img
-                alt=""
-                src="/doyi.png"
-                className="inset-0 object-cover"
-              />
-            </div>
-            <div className="w-1/2">
-              <img
-                alt=""
-                src="/funapple2.png"
-                className="inset-0 object-cover"
-              />
-            </div>
+        </div>
+        <div className="col-span-2 flex justify-center items-center">
+          <div className="w-1/2">
+            <img
+              src="/doyi.png"
+              className="inset-0 object-cover"
+            />
+          </div>
+          <div className="w-1/2">
+            <img
+              src="/funapple2.png"
+              className="inset-0 object-cover"
+            />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
